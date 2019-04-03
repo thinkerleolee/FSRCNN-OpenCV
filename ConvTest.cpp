ac9c09f480e6e986856e12b0f7b16043f79d3a4b
tinkerleo <tinkerleo@hotmail.com>
2019-04-03 16:58:28 +0800

    add:readme.md

--- ConvTest.cpp
+++ ConvTest.cpp
@@ -5,6 +5,8 @@
 #include <array>
 #include <numeric>
 #include <chrono>
+#include <unordered_map>
+
 #include "fsrcnn.h"
 #include "utils.h"
 
@@ -20,74 +22,98 @@
 using namespace cv;
 using namespace std;
 
+static void show_usage(std::string name)
+{
+	std::cerr << "Usage: " << name << " <option(s)> SOURCE-IMG" << std::endl
+		<< "Options:\n"
+		<< "\t-f,--fast True/Flase\tUse FSRCNN-s OR FSRCNN\n"
+		<< "\t-s,--scale 2/3\tSpecify the scale num"
+		<< std::endl;
+}
+
 int main(int argc, char** argv)
 {
-	/*
-		VideoCapture capture;
-	capture.open("C:\\Users\\think\\Documents\\个人文档\\Design\\FSRCNN-OpenCV\\video\\the-grandmaster_360p_10s.mp4");//VideoCapture类的方法
+	if (argc != 6) {
+		show_usage(argv[0]);
+		return 1;
+	}
+	std::unordered_map<string, string> paras;
 
+	for (int i = 1; i < argc; ++i) {
+		if (i == argc - 1) {
+			paras["source"] = String(argv[i]);
+		}
+		else {
+			paras[String(argv[i])] = String(argv[i+1]);
+			++i;
+		}
+	}
+	bool fast;
+	int scale;
+	String path;
 
-	//检查是否打开成功
-	if (!capture.isOpened())
-	{
-		cout << "视频没有打开" << endl;
+	if (paras.count("--fast") || paras.count("-f")) {
+		auto func_fast = [&](String str) {
+			if ("True" == str || "TRUE" == str || "true" == str) {
+				fast = true;
+			}
+			else if("False" == str || "FALSE" == str || "false" == str) {
+				fast = false;
+			}
+			else {
+				show_usage(argv[0]);
+				exit(1);
+			}
+		};
+		if (paras.count("--fast")) {
+			func_fast(paras["--fast"]);
+		}
+		else if(paras.count("-f")){
+			func_fast(paras["-f"]);
+		}
+	}
+	else {
+		show_usage(argv[0]);
+		return 1;
+	}
+	
+	if (paras.count("--scale") || paras.count("-s")) {
+		auto func_fast = [&](String str) {
+			if ("2" == str) {
+				scale = 2;
+			}
+			else if ("3" == str) {
+				scale = 3;
+			}
+			else {
+				show_usage(argv[0]);
+				exit(1);
+			}
+		};
+		if (paras.count("--scale")) {
+			func_fast(paras["--scale"]);
+		}
+		else if (paras.count("-s")) {
+			func_fast(paras["-s"]);
+		}
+	}
+	else {
+		show_usage(argv[0]);
+		return 1;
+	}
+	if (paras.count("source")) {
+		path = paras["source"];
+	}
+	else {
+		show_usage(argv[0]);
 		return 1;
 	}
 
+	std::cout << "fast: " << fast << std::endl;
+	std::cout << "scale: " << scale << std::endl;
+	std::cout << "source: " << path << std::endl;
 
-	//获取视频的帧速率（一般是30或者60）
-	double frame_rate = capture.get(CV_CAP_PROP_FPS);
-	cout << frame_rate << endl;
-
-
-	double codec = capture.get(CV_CAP_PROP_FOURCC);
-
-	//获取视频的总帧数目
-	long num_frame = static_cast<long>(capture.get(CV_CAP_PROP_FRAME_COUNT));
-	cout << num_frame << endl;
-
-	Mat frame;
-
-	//根据帧速率计算帧之间的等待时间，单位ms
-	int delat = 1000 / frame_rate;
-
-
-	int w = 1280;
-	int h = 720;
-
-	cv::VideoWriter out(
-		"C:\\Users\\think\\Documents\\个人文档\\Design\\FSRCNN-OpenCV\\video\\out-1.mp4", // 输出文件名
-		int(codec),  // 编码形式，使用 CV_FOURCC()宏
-		int(frame_rate), // 输出视频帧率
-		cv::Size(w, h), // 单帧图片的大小
-		true // 如果是false,可传入灰度图像 
-	);
-
-	int i = 0;
-	//循环遍历视频中的全部帧
-	while (1)
-	{
-		capture.read(frame);
-
-		if (!frame.empty())//如果读完就结束
-		{
-			out.write(fsutils::SR(frame));
-			cout << "Finish Frame:" << i++ << endl;
-		}
-		else
-		{
-			break;
-		}
-	}
-
-	capture.release();//不是必须的（由于在VideoCapture类的析构函数中已经调用了）。用于关闭视频文件
-	*/
-	if (argc != 2) {
-		std::cout << "Paramaters Error" << std::endl;
-		return 0;
-	}
 	//String path = String("C:\\Users\\think\\Documents\\个人文档\\Design\\FSRCNN-OpenCV\\x64\\Release\\lena10p.bmp");
-	String path = String(argv[1]);
 
 	Mat img = imread(path);
 	if (img.empty())
@@ -96,18 +122,22 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	int scale = 3;
-
-	FSRCNN_NORMAL sr(scale);
+	FSRCNN *sr;
+	if (fast) {
+		sr = new FSRCNN_FAST(scale);
+	}
+	else {
+		sr = new FSRCNN_NORMAL(scale);
+	}
 
 	auto t1 = std::chrono::system_clock::now();
 
-	Mat res = fsutils::SR(img, sr, scale);
+	Mat res = fsutils::SR(img, *sr, scale);
 
 	auto t2 = std::chrono::system_clock::now();
 	cout << path << endl;
 	auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-	cout << "处理时间: " << diff.count() << "ms" << endl;
+	cout << "Prosessing Time: " << diff.count() << "ms" << endl;
 	if (_access("result", 0) == -1) {
 #if defined(WIN32)
 		_mkdir("result");
@@ -117,8 +147,8 @@ int main(int argc, char** argv)
 		std::cout << "Mkdir:" << "result" << std::endl;
 	}
 
-	imwrite("result\\res_" + fsutils::GetPathOrURLShortName(path), res);
-	std::cout << "Save as " << ".\result\\res_" + fsutils::GetPathOrURLShortName(path) << std::endl;
+	imwrite("result\\res_fsrcnn_" + fsutils::GetPathOrURLShortName(path), res);
+	std::cout << "Save as " << ".\\result\\res_fsrcnn_" + fsutils::GetPathOrURLShortName(path) << std::endl;
 	int w = img.cols;
 	int h = img.rows;
 
@@ -126,7 +156,7 @@ int main(int argc, char** argv)
 	resize(img, bicubic, { w * scale,h * scale }, 0, 0, cv::INTER_CUBIC);
 
 	imwrite("result\\res_bicubic_" + fsutils::GetPathOrURLShortName(path), bicubic);
-	std::cout << "Save as " << ".\result\\res_bicubic_" + fsutils::GetPathOrURLShortName(path) << std::endl;
+	std::cout << "Save as " << ".\\result\\res_bicubic_" + fsutils::GetPathOrURLShortName(path) << std::endl;
 
 	std::cout << "Press any key to continue..." << std::endl;
 	getchar();
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# TensorConv
+# FSRCNN-OpenCV
+
+C++ implementation of the Fast Super-Resolution Convolutional Neural Network (FSRCNN).
+
+This implements two models: FSRCNN which is more accurate but slower and FSRCNN-s which is faster but less accurate. 
+
+Based on this [paper](http://mmlab.ie.cuhk.edu.hk/projects/FSRCNN.html).
+
+
+## Prerequisites
+* \>Eigen-3.3.7
+* \>OpenCV-3.4.5
+
+## Usage
+
+```bash
+Usage: FSRCNN-OpenCV.exe <option(s)> SOURCE-IMG
+Options:
+        -f,--fast True/Flase    Use FSRCNN-s OR FSRCNN
+        -s,--scale 2/3  Specify the scale num
+```
+## Result
+
+**Original Lenna image:**
+
+![](https://raw.githubusercontent.com/thinkerleolee/FSRCNN-OpenCV/master/result/comic.bmp)
+
+**FSRCNN Lenna image X3:**
+
+![](https://raw.githubusercontent.com/thinkerleolee/FSRCNN-OpenCV/master/result/res_bicubic_comic.bmp)
+
+**Bicubic Lenna image X3:**
+
+![](https://raw.githubusercontent.com/thinkerleolee/FSRCNN-OpenCV/master/result/res_fsrcnn_comic.bmp)
+
+## References
+
+- [igv/SRCNN-Tensorflow](https://github.com/igv/FSRCNN-TensorFlow)
